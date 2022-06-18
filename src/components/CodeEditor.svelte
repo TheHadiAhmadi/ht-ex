4508af9fd9cff5936930ac8b3ece7f250fd0186a
ESMAT001 <esmat10.niazi@gmail.com>
2022-06-18 21:01:44 +0430

    code editor finished

--- src/components/CodeEditor.svelte
+++ src/components/CodeEditor.svelte
@@ -1,31 +1,18 @@
 <script>
-  export let handleCodeChange;
-  export let examples;
-  export let code;
-
-  $: handleCodeChange(code);
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  export let example;
 </script>
 
-<div class="w-6/12 space-y-6 px-5 py-2 overflow-y-scroll">
-  <pre
-    contenteditable
-    bind:textContent={code}
-    class="mockup-code focus:outline-none focus:ring focus:border-green-500  p-4 pt-8 max-w-md h-36 "
-  />
-
-  {#each examples as example}
-    <pre
-      contenteditable
-      bind:textContent={example.code}
-      class="mockup-code focus:outline-none focus:ring focus:border-green-500  p-4 pt-8 max-w-md h-3/6 "
-    />
-  {/each}
-
-  {#each examples as example}
-    <pre
-      contenteditable
-      bind:textContent={example.code}
-      class="mockup-code focus:outline-none p-4 pt-8 max-w-md "
-    />
-  {/each}
-</div>
+<textarea
+  on:focus={() => {
+    dispatch("codeEditorFocused");
+  }}
+  spellcheck="false"
+  autocomplete="on"
+  on:input={(e) => {
+    dispatch("codeEditorChanged", e.target.value);
+  }}
+  bind:value={example.content}
+  class="mockup-code focus:outline-none focus:ring focus:border-green-500  p-4 pt-8 max-w-md h-3/6 "
+/>
